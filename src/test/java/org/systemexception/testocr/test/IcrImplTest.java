6969eec7900e9f8e10487414998e2f674b00290f
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-06-23 16:44:41 +0200

    fix test

--- src/test/java/org/systemexception/testocr/test/IcrImplTest.java
+++ src/test/java/org/systemexception/testocr/test/IcrImplTest.java
@@ -11,6 +11,9 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author lcappuccio
  * @date 17/06/15 17:49
@@ -20,30 +23,26 @@ public class IcrImplTest {
 	private final Icr sut = new IcrImpl();
 
 	@Test
-	public void matchWords() throws Exception {
-		File[] imageFiles = getFiles("/crops/words");
-		for (File imageFile : imageFiles) {
-			System.out.println(imageFile.getAbsolutePath() + "\t" + sut.recognize(imageFile.getAbsolutePath()));
-		}
-	}
-
-	@Test
 	public void matchNumbers50Pct() throws URISyntaxException, TesseractException {
-		File[] imageFiles = getFiles("/crops/numbers");
+		File[] imageFiles = getFiles("/numbers");
 		int matches = 0;
 		for (File imageFile : imageFiles) {
 			String readedNumber = sut.recognize(imageFile.getAbsolutePath());
-			String numberInFile = imageFile.getName().replace(".png", "");
+			String numberInFile = getFileNameWithoutPathAndExtension(imageFile.getName());
 			if (readedNumber.equals(numberInFile)) {
 				matches++;
 			}
 		}
-		assert (matches*100/imageFiles.length > 50);
+		assertTrue(matches * 100 / imageFiles.length > 50);
 	}
 
 	private File[] getFiles(String path) throws URISyntaxException {
 		URL fileUrl = this.getClass().getResource(path);
 		Path runningPath = Paths.get(fileUrl.toURI()).toAbsolutePath();
 		return runningPath.toAbsolutePath().toFile().listFiles();
+	}
+
+	private String getFileNameWithoutPathAndExtension(final String fileNamePath) {
+		return fileNamePath.substring(0, fileNamePath.lastIndexOf(".")).replaceFirst("^0+(?!$)", "");
 	}
 }
