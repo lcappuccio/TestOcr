262b1bff0797686bac2122a9f4acde1f4cc4acfe
Leonardo Cappuccio <leo@systemexception.org>
2015-06-25 21:05:48 +0200

    green: imageIsCreated

--- src/test/java/org/systemexception/testocr/test/ImageProviderTest.java
+++ src/test/java/org/systemexception/testocr/test/ImageProviderTest.java
@@ -3,6 +3,10 @@ package org.systemexception.testocr.test;
 import org.junit.Test;
 import org.systemexception.testocr.provider.ImageProvider;
 
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author leo
  * @date 24/06/15 14:32
@@ -10,10 +14,17 @@ import org.systemexception.testocr.provider.ImageProvider;
 public class ImageProviderTest {
 
 	@Test
-	public void doStuff() {
+	public void imageIsCreated() {
 		ImageProvider sut = new ImageProvider();
-		for (int i = 100; i < 200; i++) {
+		for (int i = 0; i < 10; i++) {
 			sut.drawStringAndSaveFile(String.valueOf(i));
+			assertTrue(fileExists(String.valueOf(i)));
 		}
+	}
+
+	private boolean fileExists(String fileName) {
+		String filePathAndName = System.getProperty("user.dir");
+		File imageFile = new File(filePathAndName + File.separator + fileName + ".png");
+		return imageFile.exists();
 	}
 }
